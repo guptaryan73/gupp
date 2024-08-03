1ef668204614ed8d83049a0bf7874dc25e49d419
guptaryan73 <guptaryan73@gmail.com>
2024-08-04 03:03:34 +0530

    changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-         <App />
-      </BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </React.StrictMode>,
 )
