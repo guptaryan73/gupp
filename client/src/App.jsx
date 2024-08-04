25967bdb3bd4a0410f60f51383a7fed1d14e371d
guptaryan73 <guptaryan73@gmail.com>
2024-08-04 23:39:47 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <Navbar/>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element= {<Homepage />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
       <Footer/>
